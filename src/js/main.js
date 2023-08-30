3080527821c73f6c405d9405fe7813552095600c
Nelson costa <canelsong@gmail.com>
2023-08-30 13:57:22 -0400

    start

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
+let itemMenu = document.querySelector('.navbar_menu > ul')
+let itemsMenu = document.querySelectorAll('.navbar_menu > ul > li > a')
+
+// itemMenu.addEventListener('click', (e) => {
+// 	e.preventDefault()
+// 	if(e.target.closest('.item_submenu')) {
+// 		let itemSubmenu = e.target.closest('.item_submenu');
+// 		openSubmenu(itemSubmenu)
+// 	}	
+// })
+
+// function openSubmenu(itemSubmenu) {
+// 	let ItemSubMenu = itemSubmenu.querySelector('.submenu')
+// 	console.log(ItemSubMenu);
+// }
+
+
+
+itemsMenu.forEach((item) => {
+	item.addEventListener('click', (e) => {
+		e.preventDefault()	
+		
+		item.classList.toggle('show_submenu');
+	})	
+})
+
+
 
 let btnMob = document.querySelector('.btn_mobile')
 let menuMob = document.querySelector('.navbar_menu')
